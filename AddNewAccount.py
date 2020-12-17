0891a32744a58664e2f71d6d7daf673cce988ac1
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2020-12-17 17:06:43 +0200

    Update AddNewAccount.py

--- AddNewAccount.py
+++ AddNewAccount.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 from PlaceHolder import PlaceHolder
 
 
-cluster = MongoClient("mongodb+srv://slavsup15:s9hVixpgjK1gbFM8@cluster0.hgr2y.mongodb.net/testdata?retryWrites=true&w=majority")
+cluster = MongoClient("YOUR CONNECT TO THE DATABASE (MongoDB)")
 
 db = cluster["testdata"]
 collection = db["testcoll"]
